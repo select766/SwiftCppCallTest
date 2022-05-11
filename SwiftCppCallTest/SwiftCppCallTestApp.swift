c651c2909a2b3f9428b145f003673870f9dfc2af
select766 <select766@outlook.jp>
2022-05-11 19:21:36 +0900

    Initial Commit

---
+++ SwiftCppCallTest/SwiftCppCallTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCppCallTestApp.swift
+//  SwiftCppCallTest
+//
+//  Created by Masatoshi Hidaka on 2022/05/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCppCallTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
