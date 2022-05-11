18eb9966d47821ef10c6e12c65ad59c6c1166641
select766 <select766@outlook.jp>
2022-05-11 19:28:28 +0900

    Xcodeでcsource.cを作成した時に自動生成されたもの

---
+++ SwiftCppCallTest/csource.c
@@ -0,0 +1,8 @@
+//
+//  csource.c
+//  SwiftCppCallTest
+//
+//  Created by Masatoshi Hidaka on 2022/05/11.
+//
+
+#include "csource.h"
---
+++ SwiftCppCallTest/csource.h
@@ -0,0 +1,13 @@
+//
+//  csource.h
+//  SwiftCppCallTest
+//
+//  Created by Masatoshi Hidaka on 2022/05/11.
+//
+
+#ifndef csource_h
+#define csource_h
+
+#include <stdio.h>
+
+#endif /* csource_h */
